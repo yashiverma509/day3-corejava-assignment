055eba1de43f3fdb44a336f0068891b71e76e23b
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 11:24:25 +0300

    Create InvalidCompanyException.java

---
+++ harman_q5/InvalidCompanyException.java
@@ -0,0 +1,12 @@
+package a3;
+
+
+public class InvalidCompanyException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public InvalidCompanyException(String message) {
+			super(message);
+		}
+
+
+}
